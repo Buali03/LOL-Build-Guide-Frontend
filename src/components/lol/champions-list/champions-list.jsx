50aa10adb7aa3383115c0244c4fa8b40841dfacd
Your Name <you@example.com>
2025-08-31 15:41:47 +0300

    fixed tailwind css

--- src/components/lol/champions-list/champions-list.jsx
+++ src/components/lol/champions-list/champions-list.jsx
@@ -4,16 +4,23 @@ import axios from "axios";
 
 const ChampionsList = ({ champions, baseApiURL, version }) => {
   return (
-    <div className="grid grid-cols-5 sm:grid-cols-3 md:grid-cols-5 gap-6 p-6">
+    <div className="grid grid-cols-5 gap-6 p-6">
       {champions.length
         ? champions.map((champ) => {
             console.log(champ);
             const imageURLFull = `${baseApiURL}${version}/img/champion/${champ.image.full}`;
             return (
-              <div key={champ.id}>
-                <img src={imageURLFull} />
-                <p>{champ.name}</p>
-              </div>
+              <Link
+                to={`/lolguides/champions/${champ.id}`}
+                key={champ.id}
+                className="flex flex-col items-center bg-black rounded-xl shadow-md p-4 hover:scale-105 transition-transform duration-200"
+              >
+                <img src={imageURLFull} className="w-20 h-20 rounded-lg mb-2" />
+
+                <p className="text-sm font-semibold text-gray-200">
+                  {champ.name}
+                </p>
+              </Link>
             );
           })
         : null}
